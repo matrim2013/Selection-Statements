69cb7686dddcbd869fd4a965c2da60aa1cc35c43
Lewis S <simpkinslewisa@gmail.com>
2020-08-25 12:21:51 -0400

    Selection Statements Fav Number

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput > 13)
+            {
+                Console.WriteLine("Too High!");
+            }
+            else if (userInput < 13)
+            {
+                Console.WriteLine("Too Low!");
+            }
+            else if (userInput == 13)
+            {
+                Console.WriteLine("Ding! Ding! Ding! We have a winner!");
+            }
+                    
+        }
+    }
+}
